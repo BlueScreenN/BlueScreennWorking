06575f831c1c0786318053f6a56f4f0b22a47392
OsiBK <osman_bayram-fb@hotmail.com>
2023-09-25 22:56:18 +0300

    Python 3.11.4 - Linux - NetScanner

---
+++ NetScanner/NetScanner.py
@@ -0,0 +1,25 @@
+import optparse
+import scapy.all as scapy
+
+
+def GetUserInput():
+    parseObject = optparse.OptionParser()
+    parseObject.add_option("-i","--ipadress", dest="ipAdress", help="Enter IP adress")
+    (userInput, arguments) = parseObject.parse_args()
+
+    if not userInput.ipAdress:
+        print("Enter IP adress")
+
+    return userInput
+
+def ScanMyNetwork(ip):
+    arpRequestPacket = scapy.ARP(pdst=ip)
+    #scapy.ls(scapy.ARP())
+    broadcastPacket = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    #scapy.ls(scapy.Ether())
+    combinedPacket = broadcastPacket/arpRequestPacket
+    (answeredList, unansweredList) = scapy.srp(combinedPacket,timeout=1)
+    answeredList.summary()
+
+userIpAdress = GetUserInput()
+ScanMyNetwork(userIpAdress.ipAdress)
